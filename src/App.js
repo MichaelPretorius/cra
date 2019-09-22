aa70addfb803394814cab45c8093e63ce2e406a5
Michael Pretorius <michaelpretorius96@gmail.com>
2019-09-22 22:27:55 +0200

    adding firebase dev-prod

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { signInWithGoogle } from './firebase';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={signInWithGoogle}>
+          Sign in with google
+        </button>
       </header>
     </div>
   );
---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
